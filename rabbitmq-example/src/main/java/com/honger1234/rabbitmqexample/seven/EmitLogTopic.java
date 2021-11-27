7c314f9e15e8b8a1b6dd60490b36240025e7600e
zhangting <zhangting@mengyuan.com>
2021-11-27 09:08:54 +0800

    topic交换机代码

---
+++ rabbitmq-example/src/main/java/com/honger1234/rabbitmqexample/seven/EmitLogTopic.java
@@ -0,0 +1,34 @@
+package com.honger1234.rabbitmqexample.seven;
+
+import com.honger1234.rabbitmqexample.utils.RabbitMqUtils;
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 生产者：topic交换机
+ */
+public class EmitLogTopic {
+    public static final String EXCHANGE_NAME="topic_logs";
+
+    public static void main(String[] args) throws Exception {
+        Channel channel = RabbitMqUtils.getChannel();
+        channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.TOPIC);
+        HashMap<String, String> bindingKeyMap = new HashMap<>();
+        bindingKeyMap.put("quick.orange.rabbit","被队列 Q1Q2 接收到");
+        bindingKeyMap.put("lazy.orange.elephant","被队列 Q1Q2 接收到");
+        bindingKeyMap.put("quick.orange.fox","被队列 Q1 接收到");
+        bindingKeyMap.put("lazy.brown.fox","被队列 Q2 接收到");
+        bindingKeyMap.put("lazy.pink.rabbit","虽然满足两个绑定但只被队列 Q2 接收一次");
+        bindingKeyMap.put("quick.brown.fox","不匹配任何绑定不会被任何队列接收到会被丢弃");
+        bindingKeyMap.put("quick.orange.male.rabbit","是四个单词不匹配任何绑定会被丢弃");
+        bindingKeyMap.put("lazy.orange.male.rabbit","是四个单词但匹配 Q2");
+        for (Map.Entry<String, String> stringStringEntry : bindingKeyMap.entrySet()) {
+            channel.basicPublish(EXCHANGE_NAME,stringStringEntry.getKey(),null,stringStringEntry.getValue().getBytes(StandardCharsets.UTF_8));
+            System.out.println("生产者发送的消息:"+stringStringEntry.getValue());
+        }
+    }
+}
---
+++ rabbitmq-example/src/main/java/com/honger1234/rabbitmqexample/seven/ReceiveLogsTopic01.java
@@ -0,0 +1,26 @@
+package com.honger1234.rabbitmqexample.seven;
+
+import com.honger1234.rabbitmqexample.utils.RabbitMqUtils;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DeliverCallback;
+
+/**
+ * topic交换机的消费者
+ */
+public class ReceiveLogsTopic01 {
+    private static final String EXCHANGE_NAME = "topic_logs";
+    public static void main(String[] argv) throws Exception {
+        Channel channel = RabbitMqUtils.getChannel();
+        channel.exchangeDeclare(EXCHANGE_NAME, "topic");
+        //声明 Q1 队列与绑定关系
+        String queueName="Q1";
+        channel.queueDeclare(queueName, false, false, false, null);
+        channel.queueBind(queueName, EXCHANGE_NAME, "*.orange.*");
+        System.out.println("等待接收消息........... ");
+        DeliverCallback deliverCallback = (consumerTag, delivery) ->{
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" 接收队列 :"+queueName+" 绑定键:"+delivery.getEnvelope().getRoutingKey()+",消息:"+message);
+        };
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {});
+    }
+}
