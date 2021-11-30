f80f90cb7d13faf7cbb1536a4621d955259a776d
zhangting <zhangting@mengyuan.com>
2021-11-30 15:47:02 +0800

    juc解决ArrayList的三种方案：1.vector 2.collections 3copyonWriteArrayList

---
+++ thread-learning/src/main/java/com/honger1234/threadlearning/juc/lock/ThreadDemo4.java
@@ -0,0 +1,61 @@
+package com.honger1234.threadlearning.juc.lock;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.stream.Collectors;
+
+/**
+ * list集合线程不安全
+ */
+public class ThreadDemo4 {
+    public static void main(String[] args) {
+        //创建ArrayList集合
+//        List<String> list = new ArrayList<>();
+        // Vector解决
+//        Vector<String> list = new Vector<>();
+
+        //Collections解决
+//        Collection<String> list = Collections.synchronizedCollection(new ArrayList<>());
+
+        // CopyOnWriteArrayList解决
+        CopyOnWriteArrayList<String> list = new CopyOnWriteArrayList<>();
+//        List<String> list = new CopyOnWriteArrayList<>();
+        for (int i = 0; i <30; i++) {
+            new Thread(()->{
+                //向集合添加内容
+                list.add(UUID.randomUUID().toString().substring(0,8));
+                //从集合获取内容
+                System.out.println(list.toString());
+            },String.valueOf(i)).start();
+        }
+
+        //演示Hashset
+//        Set<String> set = new HashSet<>();
+
+//        Set<String> set = new CopyOnWriteArraySet<>();
+//        for (int i = 0; i <30; i++) {
+//            new Thread(()->{
+//                //向集合添加内容
+//                set.add(UUID.randomUUID().toString().substring(0,8));
+//                //从集合获取内容
+//                System.out.println(set);
+//            },String.valueOf(i)).start();
+//        }
+
+        //演示HashMap
+//        Map<String,String> map = new HashMap<>();
+
+//        Map<String,String> map = new ConcurrentHashMap<>();
+//        for (int i = 0; i <30; i++) {
+//            String key = String.valueOf(i);
+//            new Thread(()->{
+//                //向集合添加内容
+//                map.put(key,UUID.randomUUID().toString().substring(0,8));
+//                //从集合获取内容
+//                System.out.println(map);
+//            },String.valueOf(i)).start();
+//        }
+    }
+}
