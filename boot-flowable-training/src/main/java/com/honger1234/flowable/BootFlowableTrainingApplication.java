f9f0390b8e9c1b5ed77d883fe3f3407945cbbcc7
张庭 <183162113@qq.com>
2023-06-09 18:10:12 +0800

    feat:springboot集成flowable

---
+++ boot-flowable-training/src/main/java/com/honger1234/flowable/BootFlowableTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.honger1234.flowable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootFlowableTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootFlowableTrainingApplication.class, args);
+    }
+
+}
---
+++ boot-flowable-training/src/test/java/com/honger1234/flowable/BootFlowableTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.honger1234.flowable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootFlowableTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
