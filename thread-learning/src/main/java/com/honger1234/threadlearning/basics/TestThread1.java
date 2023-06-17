8a42a619e3e4329e5a6b9ec12021967382280d39
zhangting <zhangting@mengyuan.com>
2023-06-17 11:46:22 +0800

    feat：使用继承Thread类创建线程

--- thread-learning/src/main/java/com/honger1234/threadlearning/basics/TestThread1.java
+++ thread-learning/src/main/java/com/honger1234/threadlearning/basics/TestThread1.java
@@ -1,7 +1,11 @@
 package com.honger1234.threadlearning.basics;
 
+/**
+ * 使用继承Thread类创建线程
+ */
 public class TestThread1 extends Thread{
 
+    @Override
     public void run(){
         for (int i = 0; i < 10; i++) {
             System.out.println("我在撸代码"+i);
