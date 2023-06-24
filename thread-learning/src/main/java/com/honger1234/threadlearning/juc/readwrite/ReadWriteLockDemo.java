bc863f2aa616860d2ffae4abb51c3c8c0a0bf9da
zhangting <zhangting@mengyuan.com>
2023-06-24 12:22:13 +0800

    feat：使用ReadWriteLock 读写锁

--- thread-learning/src/main/java/com/honger1234/threadlearning/juc/readwrite/ReadWriteLockDemo.java
+++ thread-learning/src/main/java/com/honger1234/threadlearning/juc/readwrite/ReadWriteLockDemo.java
@@ -7,24 +7,33 @@ import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 //资源类
+
+/**
+ * 独占锁(写锁) 一次只能被一个线程占有
+ * 共享锁(读锁) 多个线程可以同时占有
+ * ReadWriteLock
+ * 读 - 读 可以共存
+ * 读 - 写 不能共存
+ * 写 - 写 不能共存
+ * */
 class MyCache {
     //创建map集合
-    private  volatile Map<String,Object> map = new HashMap<>();
+    private volatile Map<String, Object> map = new HashMap<>();
 
     //创建读写锁对象
-    private ReadWriteLock readWriteLock=new ReentrantReadWriteLock();
+    private ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
 
     //放数据
-    public void put(String key,Object value) {
+    public void put(String key, Object value) {
         //添加写锁
         readWriteLock.writeLock().lock();
         try {
-            System.out.println(Thread.currentThread().getName()+" 正在写操作"+key);
+            System.out.println(Thread.currentThread().getName() + " 正在写操作" + key);
             //暂停一会
             TimeUnit.MILLISECONDS.sleep(300);
             //放数据
-            map.put(key,value);
-            System.out.println(Thread.currentThread().getName()+" 写完了"+key);
+            map.put(key, value);
+            System.out.println(Thread.currentThread().getName() + " 写完了" + key);
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
@@ -39,11 +48,11 @@ class MyCache {
         readWriteLock.readLock().lock();
         Object result = null;
         try {
-            System.out.println(Thread.currentThread().getName()+" 正在读取操作"+key);
+            System.out.println(Thread.currentThread().getName() + " 正在读取操作" + key);
             //暂停一会
             TimeUnit.MILLISECONDS.sleep(300);
             result = map.get(key);
-            System.out.println(Thread.currentThread().getName()+" 取完了"+key);
+            System.out.println(Thread.currentThread().getName() + " 取完了" + key);
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
@@ -58,21 +67,21 @@ public class ReadWriteLockDemo {
     public static void main(String[] args) throws InterruptedException {
         MyCache myCache = new MyCache();
         //创建线程放数据
-        for (int i = 1; i <=5; i++) {
+        for (int i = 1; i <= 5; i++) {
             final int num = i;
-            new Thread(()->{
-                myCache.put(num+"",num+"");
-            },String.valueOf(i)).start();
+            new Thread(() -> {
+                myCache.put(num + "", num + "");
+            }, String.valueOf(i)).start();
         }
 
         TimeUnit.MILLISECONDS.sleep(300);
 
         //创建线程取数据
-        for (int i = 1; i <=5; i++) {
+        for (int i = 1; i <= 5; i++) {
             final int num = i;
-            new Thread(()->{
-                myCache.get(num+"");
-            },String.valueOf(i)).start();
+            new Thread(() -> {
+                myCache.get(num + "");
+            }, String.valueOf(i)).start();
         }
     }
 }
